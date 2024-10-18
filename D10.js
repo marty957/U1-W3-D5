dc7106360b6dd88fa167febf9839a0b433a582c0
marty957 <fantuzzimartina@yahoo.it>
2024-10-18 16:51:02 +0200

    es19 corect

--- D10.js
+++ D10.js
@@ -370,7 +370,15 @@ console.log(searchAndDivide("Lord"));
 /* ESERCIZIO 19
   Scrivi una funzione chiamata "removeIndex" che riceve un numero come parametro e ritorna l'array "movies" fornito privo dell'elemento nella posizione ricevuta come parametro.
 */
-const removeIndex = function (n) {};
+const removeIndex = function (n) {
+  const newMoviesArr = [];
+  for (let i = 0; i < movies.length; i++) {
+    if (i !== n) {
+      newMoviesArr.push(movies[i]);
+    }
+  }
+  return newMoviesArr;
+};
 
 console.log(removeIndex(5));
 // DOM (nota: gli elementi che selezionerai non si trovano realmente nella pagina)
